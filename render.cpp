16463787577b5969c2eff314faa8716aafd04f10
Pedro Henrique <peedrohs.mesquita@outlook.com>
2024-12-24 10:37:07 -0300

    Progresso animação

--- render.cpp
+++ render.cpp
@@ -9,7 +9,10 @@
 void converterIntParaColor(Color &cor, int corInt);
 void escurecerCor(Color &corOriginal, Color &cor, float fatorEscurecer);
 void desenharSuporte(const Tabuleiro &tabuleiro);
+void desenharBaseTabuleiro(const Tabuleiro &tabuleiro);
 void desenharPecasTabuleiro(const Tabuleiro &tabuleiro);
+Texture2D& carregarTexturaTabuleiro();
+void descarregarTexturaTabuleiro();
 
 void desenharTabuleiro(const Tabuleiro &tabuleiro, Mouse &mouse) {
     BeginDrawing();
@@ -17,6 +20,7 @@ void desenharTabuleiro(const Tabuleiro &tabuleiro, Mouse &mouse) {
     ClearBackground(GRAY);
     SetMouseCursor(mouse.tipoCursor);
     desenharSuporte(tabuleiro);
+    desenharBaseTabuleiro(tabuleiro);
     desenharPecasTabuleiro(tabuleiro);
 
     EndDrawing();
@@ -37,11 +41,12 @@ void escurecerCor(Color &corOriginal, Color &cor, float fatorEscurecer) {
 }
 
 void desenharSuporte(const Tabuleiro &tabuleiro) {
-    int deslocar;
+    int deslocar = 5;
     Rectangle partePeca, partePecaSombra, pilarEsq, pilarDir;
     Color cor, sombra;
 
     deslocar = 5;
+    /*
     partePeca = {
         TABULEIRO_PECAS_TAM_X1, TABULEIRO_PECAS_TAM_Y1,
         TABULEIRO_PECAS_TAM_X2 - TABULEIRO_PECAS_TAM_X1,
@@ -53,6 +58,7 @@ void desenharSuporte(const Tabuleiro &tabuleiro) {
         TABULEIRO_PECAS_TAM_X2 - TABULEIRO_PECAS_TAM_X1 + deslocar*2,
         TABULEIRO_PECAS_TAM_Y2 - TABULEIRO_PECAS_TAM_Y1 + deslocar*2
     };
+    */
     pilarEsq = {
         0, TABULEIRO_DESLOCAY,
         TABULEIRO_DESLOCAX - deslocar, 768 - 90
@@ -66,6 +72,7 @@ void desenharSuporte(const Tabuleiro &tabuleiro) {
 
     DrawRectangleRounded(pilarEsq, 0.35f, 0, cor);
     DrawRectangleRounded(pilarDir, 0.35f, 0, cor);
+    /*
     DrawRectangleRounded(partePecaSombra, 0.1, 10, sombra);
     DrawRectangleRounded(partePeca, 0.1f, 10, cor);
     escurecerCor(cor, sombra, 0.95f);
@@ -79,6 +86,16 @@ void desenharSuporte(const Tabuleiro &tabuleiro) {
         };
         DrawRectangleRounded(pecasArea, 0.3f, 10, sombra);
     }
+    */
+}
+
+void desenharBaseTabuleiro(const Tabuleiro &tabuleiro) {
+    Texture2D textura = carregarTexturaTabuleiro();
+    int desloca = 5;
+    Color cor;
+
+    converterIntParaColor(cor, tabuleiro.corSuporte);
+    DrawTextureEx(textura, (Vector2){TABULEIRO_PECAS_TAM_X1 - desloca, TABULEIRO_PECAS_TAM_Y1 - desloca}, 0.0f, 1.0f, cor);
 }
 
 void desenharPecasTabuleiro(const Tabuleiro &tabuleiro) {
@@ -101,4 +118,16 @@ void desenharPecasTabuleiro(const Tabuleiro &tabuleiro) {
             }
         }
     }
+}
+
+Texture2D& carregarTexturaTabuleiro() {
+    static Texture2D textura = LoadTexture("assets/base.png");
+
+    return textura;
+}
+
+void descarregarTexturaTabuleiro() {
+    Texture2D textura = carregarTexturaTabuleiro();
+
+    UnloadTexture(textura);
 }

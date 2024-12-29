0d57ab47d1dd1e036273d50da3bf9e5be2bf1734
Pedro Henrique <peedrohs.mesquita@outlook.com>
2024-12-29 14:23:32 -0300

    Remoção da fila de ações

--- jogador.cpp
+++ jogador.cpp
@@ -18,22 +18,3 @@ void jogadorEstadoInicial(Jogador &jogador, int id, int pecas, int cor, char *no
 void removerPeca(Jogador &jogador) {
     jogador.pecas--;
 }
-
-void filaAcoesEstadoInicial(FilaAcoes &fila) {
-    fila.fim = -1;
-    fila.tam = 0;
-}
-
-void adicionarAcao(FilaAcoes &fila, Jogador *autor, int l, int c, float x, float y, float yf, float v, float a) {
-    fila.fim = fila.fim + 1;
-    fila.acao[fila.fim].autor = autor;
-    fila.acao[fila.fim].linha = l;
-    fila.acao[fila.fim].col = c;
-    fila.acao[fila.fim].animacao.x = x;
-    fila.acao[fila.fim].animacao.y = y;
-    fila.acao[fila.fim].animacao.yf = yf;
-    fila.acao[fila.fim].animacao.v = v;
-    fila.acao[fila.fim].animacao.a = a;
-    fila.acao[fila.fim].concluida = false;
-    fila.tam++;
-}
--- jogador.h
+++ jogador.h
@@ -16,33 +16,9 @@ struct Jogador {
     char nome[NOME_TAM+1];
 };
 
-struct AnimacaoPeca {
-    float x;
-    float y;
-    float yf;
-    float v;
-    float a;
-};
-
-struct Acao {
-    Jogador *autor;
-    int linha;
-    int col;
-    bool concluida;
-    AnimacaoPeca animacao;
-};
-
-struct FilaAcoes {
-    Acao acao[6*7];
-    int fim;
-    int tam;
-};
-
 void jogadorEstadoInicial(Jogador &jogador, int id, int pecas, int cor, char *nome);
 void removerPeca(Jogador &Jogador);
-void filaAcoesEstadoInicial(FilaAcoes &fila);
-void adicionarAcao(FilaAcoes &fila, Jogador *autor, int l, int c, float x, float y, float yf, float v, float a);
-void removerAcao(FilaAcoes &fila);
+
 
 
 #endif // JOGADOR_H

9baa82461d5f4ffc1cc09c0e2cfcf033c1b290b3
Pedro Henrique <peedrohs.mesquita@outlook.com>
2025-01-02 10:58:45 -0300

    Adicionando fontes

--- interface.cpp
+++ interface.cpp
@@ -5,3 +5,64 @@
  * deverão ser responsáveis pela lógica de elementos gerais da interface.
  */
 #include "interface.h"
+
+Fontes &obterFontes() {
+    static Fontes fontes;
+
+    return fontes;
+}
+
+void inicializarCaixa(Caixa &caixa, Rectangle &retangulo, float redondeza, int segmentos, Color cor) {
+    caixa.retangulo = retangulo;
+    caixa.redondeza = redondeza;
+    caixa.segmentos = segmentos;
+    caixa.cor = cor;
+}
+
+void inicializarTexto(Texto &texto, Vector2 &posicao, char *conteudo, float tam, float espac, Color cor, Font &fonte) {
+    texto.posicao = posicao;
+    texto.conteudo = conteudo;
+    texto.tamanho = tam;
+    texto.espacamento = espac;
+    texto.cor = cor;
+    texto.fonte = fonte;
+}
+
+void carregarFonte() {
+    Fontes &fontes = obterFontes();
+
+    //fontes.openSansLight = LoadFontEx(OPEN_SANS_LIGHT, 14.0f, nullptr, 0);
+    fontes.openSansSemibold16 = LoadFontEx(OPEN_SANS_SEMIBOLD, 16.0f, nullptr, 0);
+    fontes.openSansSemibold32 = LoadFontEx(OPEN_SANS_SEMIBOLD, 32.0f, nullptr, 0);
+}
+
+bool mouseSobreCaixa(const Caixa &caixa, const Mouse &mouse) {
+    return mouse.x >= caixa.retangulo.x && mouse.x <= (caixa.retangulo.x + caixa.retangulo.width) &&
+           mouse.y >= caixa.retangulo.y && mouse.y <= (caixa.retangulo.y + caixa.retangulo.height);
+}
+
+Font& obterOpenSansLight() {
+    Fontes &fontes = obterFontes();
+
+    return fontes.openSansLight;
+}
+
+Font& obterOpenSansSemiBold16() {
+    Fontes &fontes = obterFontes();
+
+    return fontes.openSansSemibold16;
+}
+
+Font& obterOpenSansSemiBold32() {
+    Fontes &fontes = obterFontes();
+
+    return fontes.openSansSemibold32;
+}
+
+void descarregarFonte() {
+    Fontes &fontes = obterFontes();
+
+    //UnloadFont(fontes.openSansLight);
+    UnloadFont(fontes.openSansSemibold16);
+    UnloadFont(fontes.openSansSemibold32);
+}
--- interface.h
+++ interface.h
@@ -8,6 +8,43 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
+#include "mouse.h"
+#include "raylib.h"
+
+#define COR_FUNDO CLITERAL(Color) {255, 251, 247, 255}
+#define OPEN_SANS_LIGHT "assets/fonts/open-sans/OpenSans-Light.ttf"
+#define OPEN_SANS_SEMIBOLD "assets/fonts/open-sans/OpenSans-Semibold.ttf"
+
+struct Fontes {
+    Font openSansLight;
+    Font openSansSemibold16;
+    Font openSansSemibold32;
+};
+
+struct Texto {
+    Vector2 posicao;
+    char *conteudo;
+    float tamanho;
+    float espacamento;
+    Color cor;
+    Font fonte;
+};
+
+struct Caixa {
+    Rectangle retangulo;
+    float redondeza;
+    int segmentos;
+    Color cor;
+};
+
+void inicializarCaixa(Caixa &caixa, Rectangle &retangulo, float redondeza, int segmentos, Color cor);
+void inicializarTexto(Texto &texto, Vector2 &posicao, char *conteudo, float tam, float espac, Color cor, Font &fonte);
+void carregarFonte();
+bool mouseSobreCaixa(const Caixa &caixa, const Mouse &mouse);
+Font& obterOpenSansLight();
+Font& obterOpenSansSemiBold16();
+Font& obterOpenSansSemiBold32();
+void descarregarFonte();
 
 
 #endif // INTERFACE_H
